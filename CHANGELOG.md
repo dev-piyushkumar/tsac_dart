bb6604444a72c2d0fdbc906538e9ac4787288fae
piyush <piyush@muvi.com>
2019-08-31 23:42:53 +0530

    foundation and changelog fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ## 0.0.1
 
-* Initialization
+* Base initialization
--- lib/src/tsac_controller.dart
+++ lib/src/tsac_controller.dart
@@ -1,4 +1,4 @@
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
 import 'types.dart';
 
 class Task {
